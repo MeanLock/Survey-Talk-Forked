e36333db769d09c9b3b924c9f432fd549d14892e
ThinhTQP <thinhtqpse181526@fpt.edu.vn>
2025-07-17 12:46:22 +0700

    fix

--- survey-talk-frontend/survey-talk-platform-web/dist/assets/index-DxNrGn2C.js
+++
@@ -1,1 +0,0 @@
-import{j as e,P as r}from"./index-jdPKODjX.js";const s=()=>e.jsx(e.Fragment,{children:e.jsx(r,{})});export{s as default};
--- survey-talk-frontend/survey-talk-platform-web/dist/assets/index-vUtrjdXZ.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as r}from"./index-jdPKODjX.js";const a=()=>e.jsx(r,{children:e.jsx("div",{className:"",children:"MySurvey"})});export{a as default};
